3491b5440e1c39b86598ebb0e46d57d31a1e7dd3
pedrokohler <pedrokohlerbh@gmail.com>
2021-07-27 03:57:40 -0300

    feat: add unsubscription

--- src/app/components/quote-item/quote-item.component.ts
+++ src/app/components/quote-item/quote-item.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { timer } from 'rxjs';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
 import { QuoteService } from 'src/app/services/quote.service';
 import { ProcessedQuote, processedQuoteInitialValues } from 'src/app/interfaces/ProcessedQuote.interface';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -9,9 +9,10 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './quote-item.component.html',
   styleUrls: ['./quote-item.component.css']
 })
-export class QuoteItemComponent implements OnInit{
+export class QuoteItemComponent implements OnInit, OnDestroy{
   @Input() quote: ProcessedQuote = processedQuoteInitialValues;
   @Output() onDeleteQuote: EventEmitter<ProcessedQuote> = new EventEmitter();
+  timerSubscription: Subscription = new Subscription();
   faTimes = faTimes;
 
   constructor(private quoteService: QuoteService) {}
@@ -20,11 +21,17 @@ export class QuoteItemComponent implements OnInit{
     this.triggerNextPolling();
   }
 
+  ngOnDestroy(): void {
+    this.timerSubscription.unsubscribe();
+  }
+
   triggerNextPolling(){
-    return timer(this.quote.pollingInterval).subscribe(_ => this.updateQuote.bind(this)());
+    console.log('triggerNextPolling');
+    this.timerSubscription = timer(this.quote.pollingInterval).subscribe(_ => this.updateQuote.bind(this)());
   }
 
   updateQuote() {
+    console.log('updateQuote');
     const observable =  this.quoteService.updateQuote(this.quote);
     observable.subscribe(quote => {
       this.quote = quote;
